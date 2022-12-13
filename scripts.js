1c3a41651c53266d03b0070928bdfa4b4de39f60
aegis945 <artiom.lekicev@gmail.com>
2022-12-14 00:58:40 +0200

    add function that randomly returns rock,paper or scissors

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
+const computerChoices = ["Rock", "Paper", "Scissors"];
 
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    return computerChoices[randomChoice];
+}
