9698b91b6f62709185443e22150ae2e6d577c2a0
aegis945 <artiom.lekincev@gmail.com>
2022-12-14 16:56:11 +0200

    check whether user's input is valid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 const computerChoices = ["Rock", "Paper", "Scissors"];
 
+/* Randomly generates a computer choice */
 function getComputerChoice() {
-    let randomChoice = Math.floor(Math.random() * 3);
-    return computerChoices[randomChoice];
+    var randomChoice = Math.floor(Math.random() * 3);
+    var computerSelection = computerChoices[randomChoice];
+    return computerSelection;
+}
+
+var playerSelection = prompt("Please type in your choice.").toLowerCase();
+/*Checks whether the user's choice is valid */
+while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+    var playerSelection = prompt(playerSelection + " isn't a valid choice. Please type in rock, paper or scissors.").toLowerCase();
 }
