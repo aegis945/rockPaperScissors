a9ff445fedb02512eebf72eac3cb4114b1dc13a9
aegis945 <artiom.lekincev@gmail.com>
2022-12-16 02:01:23 +0200

    prettified how game looks in the console

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 playerScore = 0;
 computerScore = 0;
-var choices = ["rock", "paper", "scissors"];
-
+const choices = ["rock", "paper", "scissors"];
+/*main game function that actually plays the game*/
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        playRound();
+        console.log("You chose " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1));
+        console.log("Opponent chose " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1));
+        console.log(checkRoundWinner());
+        console.log("______________________________________")
+    }
+}
 /* Randomly generates a computer choice */
 function getComputerSelection() {
     return choices[Math.floor(Math.random() * 3)];
@@ -30,21 +39,17 @@ function playRound() {
     playerChoice = getPlayerSelection();
 }
 
-function game() {
-    playRound();
-    console.log(checkWinner());
-}
 /*check who is the winner */
-function checkWinner() {
+function checkRoundWinner() {
     if (computerChoice == playerChoice) {
         return "It's a tie";
     } else if
         ((playerChoice == "rock" && computerChoice == "scissors") ||
         (playerChoice == "paper" && computerChoice == "rock") ||
         (playerChoice == "scissors" && computerChoice == "paper")) {
-        return "You win!" + playerChoice + " beats " + computerChoice;
+        return "You win this round! " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase() + " beats " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
     } else {
-        return "You lose!" + computerChoice + " beats " + playerChoice;
+        return "You lose this round! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1).toLowerCase() + " beats " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
     }
 }
 
