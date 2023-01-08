42135d0ae8633c232804a70aa5fd79c6f0103e53
aegis945 <artiom.lekicev@gmail.com>
2023-01-08 18:10:49 +0200

    display scores

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-playerScore = 0;
-computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 const choices = ["rock", "paper", "scissors"];
 /*game starts when user clicks on an image */
 function startGame() {
@@ -22,40 +22,38 @@ function playRound(playerChoice) {
     computerChoice = getComputerSelection();
     console.log(playerChoice);
     console.log(computerChoice);
-    checkRoundWinner(playerChoice, computerChoice);
+    logRoundResults(playerChoice, computerChoice);
+    displayScores();
 }
 
 
-function checkRoundWinner(playerChoice, computerChoice) {
+function logRoundResults(playerChoice, computerChoice) {
     let ul = document.getElementById("list");
     let li = document.createElement("li");
 
     if (computerChoice == playerChoice) {
         li.appendChild(document.createTextNode("It's a tie!"));
         ul.appendChild(li);
+        document.querySelector(".roundContainer").textContent = "It's a tie!";
     } else if
         ((playerChoice == "rock" && computerChoice == "scissors") ||
         (playerChoice == "paper" && computerChoice == "rock") ||
         (playerChoice == "scissors" && computerChoice == "paper")) {
         li.appendChild(document.createTextNode("You win this round! " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase() + " beats " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)));
         ul.appendChild(li);
+        document.querySelector(".roundContainer").textContent = "You win this round!";
+        playerScore++;
     } else {
         li.appendChild(document.createTextNode("You lose this round! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1).toLowerCase() + " beats " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)));
         ul.appendChild(li);
+        document.querySelector(".roundContainer").textContent = "You lose this round!";
+        computerScore++;
     }
 }
 
-function countScores() {
-    let result = checkRoundWinner();
-    if (result.includes("win")) {
-        return playerScore++;
-    } else if (result.includes("lose")) {
-        return computerScore++;
-    }
-}
-
-function logRoundResults() {
-
+function displayScores() {
+    document.querySelector(".playerScore").textContent = `Player score: ${playerScore}`;
+    document.querySelector(".computerScore").textContent = `Computer score: ${computerScore}`;
 }
 
 function finalWinner() {
