e895ed7b2dde093889a7ba8a17067ca5fc4ff9fb
aegis945 <artiom.lekicev@gmail.com>
2023-01-08 14:18:28 +0200

    added event listener to every player img

--- scripts.js
+++ scripts.js
@@ -1,53 +1,28 @@
 playerScore = 0;
 computerScore = 0;
 const choices = ["rock", "paper", "scissors"];
-/*main game function that actually plays the game*/
-function game() {
-    if (playerScore == 3 || computerScore == 3) {
-        return console.log(finalWinner());
-    } else {
-        playRound();
-        console.log("Round " + i);
-        console.log("You chose: " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1));
-        console.log("Opponent chose: " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1));
-        console.log(checkRoundWinner());
-        console.log("______________________________________");
-        countScores();
-    }
-}
-if (playerScore === computerScore) {
-    return console.log("It's a tie! Nobody won.");
-} else {
-    return console.log(finalWinner());
-}
+
+/*game starts when user clicks on an image */
+const imgs = document.querySelectorAll("img");
+imgs.forEach(img => {
+    img.addEventListener("click", e => {
+        if (img.id) {
+            console.log(img.id);
+        }
+    })
+})
+
 /* Randomly generates a computer choice */
 function getComputerSelection() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-/*get users input and filters it*/
-function getPlayerSelection() {
-    let playerSelection = prompt("Please type in Rock, Paper or Scissors.");
-    while (playerSelection == null) {
-        playerSelection = prompt("Please type in Rock, Paper or Scissors.");
-    }
-    while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-        playerSelection = prompt(playerSelection + " isn't a valid choice. Please type in Rock, Paper or Scissors (capitalization doesn't matter).");
-        while (playerSelection == null) {
-            playerSelection = prompt("Please type in Rock, Paper or Scissors.").toLowerCase();
-        }
-        playerSelection = playerSelection.toLowerCase();
-    }
-    playerSelection = playerSelection.toLowerCase();
-    return playerSelection;
-}
-/*Play a single round */
 function playRound() {
     computerChoice = getComputerSelection();
     playerChoice = getPlayerSelection();
 }
 
-/*check who is the winner */
+
 function checkRoundWinner() {
     if (computerChoice == playerChoice) {
         return "It's a tie!";
@@ -71,11 +46,20 @@ function countScores() {
 }
 
 function finalWinner() {
-    if (playerScore > computerScore) {
+    if (playerScore === computerScore) {
+        return console.log("It's a tie! Nobody won.");
+    } else if (playerScore > computerScore) {
         return "Congratulations! You are the winner!";
     } else {
         return "You lost! Computer is the winner.";
     }
 }
 
-setTimeout(() => { game(); }, 5000);
+/* if (playerScore == 3 || computerScore == 3) {
+    return console.log(finalWinner());
+} else {
+    console.log("You chose: " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1));
+    console.log("Opponent chose: " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1));
+    console.log(checkRoundWinner());
+    countScores();
+} */
