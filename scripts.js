96b243e0bd5700e2707b8256bd60ab200694d740
aegis945 <artiom.lekincev@gmail.com>
2023-01-08 20:57:00 +0200

    added reset game function and fixed some bugs

--- scripts.js
+++ scripts.js
@@ -19,33 +19,40 @@ function getComputerSelection() {
 }
 
 function playRound(playerChoice) {
-    computerChoice = getComputerSelection();
-    console.log(playerChoice);
-    console.log(computerChoice);
+    let computerChoice = getComputerSelection();
     logRoundResults(playerChoice, computerChoice);
     displayScores();
+    if (playerScore === 3 || computerScore === 3) {
+        finalWinner();
+        resetGame();
+    }
+    if (document.querySelectorAll("li").length >= 5) {
+        displayScores();
+        finalWinner();
+        resetGame();
+    }
 }
 
-
 function logRoundResults(playerChoice, computerChoice) {
-    let ul = document.getElementById("list");
+    let ol = document.getElementById("list");
     let li = document.createElement("li");
+    li.setAttribute("class", "li");
 
     if (computerChoice == playerChoice) {
         li.appendChild(document.createTextNode("It's a tie!"));
-        ul.appendChild(li);
+        ol.appendChild(li);
         document.querySelector(".roundContainer").textContent = "It's a tie!";
     } else if
         ((playerChoice == "rock" && computerChoice == "scissors") ||
         (playerChoice == "paper" && computerChoice == "rock") ||
         (playerChoice == "scissors" && computerChoice == "paper")) {
         li.appendChild(document.createTextNode("You win this round! " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase() + " beats " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)));
-        ul.appendChild(li);
+        ol.appendChild(li);
         document.querySelector(".roundContainer").textContent = "You win this round!";
         playerScore++;
     } else {
         li.appendChild(document.createTextNode("You lose this round! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1).toLowerCase() + " beats " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)));
-        ul.appendChild(li);
+        ol.appendChild(li);
         document.querySelector(".roundContainer").textContent = "You lose this round!";
         computerScore++;
     }
@@ -58,20 +65,30 @@ function displayScores() {
 
 function finalWinner() {
     if (playerScore === computerScore) {
-        return console.log("It's a tie! Nobody won.");
+        document.querySelector(".roundContainer").textContent = "It's a tie! Nobody won.";
     } else if (playerScore > computerScore) {
-        return "Congratulations! You are the winner!";
+        document.querySelector(".roundContainer").textContent = "Congratulations! You are the winner!";
     } else {
-        return "You lost! Computer is the winner.";
+        document.querySelector(".roundContainer").textContent = "You lost! Computer is the winner.";
     }
 }
 
+function resetGame() {
+    document.querySelector(".resetGame").style.visibility = "visible";
+    const button = document.querySelector(".resetGame");
+    button.addEventListener("click", () => {
+        document.querySelector(".roundContainer").textContent = "Round Log";
+        document.querySelector(".playerScore").textContent = "Player Score:";
+        document.querySelector(".computerScore").textContent = "ComputerScore";
+        playerScore = 0;
+        computerScore = 0;
+
+        let listElements = document.querySelectorAll("li");
+        for (let i = 0; (li = listElements[i]); i++) {
+            li.parentNode.removeChild(li);
+        }
+        document.querySelector(".resetGame").style.visibility = "hidden";
+    })
+}
+
 startGame();
-/* if (playerScore == 3 || computerScore == 3) {
-    return console.log(finalWinner());
-} else {
-    console.log("You chose: " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1));
-    console.log("Opponent chose: " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1));
-    console.log(checkRoundWinner());
-    countScores();
-} */
